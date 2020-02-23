3c3c8c25846f0f8c7b7efb8b45c40507bf8ca441
nick <nicolas_7979@yahoo.ca>
2020-02-23 12:14:59 -0500

    Restrict app to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'utilities/application_constants.dart' as application_constants;
 import 'widgets/screens/home_screen.dart';
 
-void main() => runApp(Application());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(Application());
+  });
+}
 
 class Application extends StatelessWidget {
   @override
