4b5b530792b3b02a5f77432ec0d16818f81ad3ca
Daniel <danielwiktorczyk@gmail.com>
2020-02-26 15:27:47 -0500

    Updated smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,37 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Application());
-
+    final application = Application();
+    await tester.pumpWidget(application);
     expect(find.byType(GoogleMap), findsOneWidget);
     expect(find.byType(FloatingActionButton), findsNWidgets(2));
+
     var switch_views = find.byIcon(Icons.sync);
-    var my_location = find.byIcon(Icons.gps_fixed);
     await tester.tap(switch_views);
+    await tester.pump();
+
+    var my_location = find.byIcon(Icons.gps_fixed);
     await tester.tap(my_location);
+    await tester.pump();
+
+    var menu = find.byIcon(Icons.menu);
+    await tester.tap(menu);
+    await tester.pump();
+
+    var sgw_campus = find.text("SGW Campus");
+    await tester.tap(sgw_campus);
+    await tester.pump();
+    var sgw_campus_buildings = find.byType(ListTile);
+    await tester.tap(sgw_campus_buildings.at(1));
+    await tester.pump();
+
+    await tester.tap(menu);
+    await tester.pump();
+    var loyola_campus = find.text("Loyola Campus");
+    await tester.tap(loyola_campus);
+    await tester.pump();
+    var loyola_campus_buildings = find.byType(ListTile);
+    await tester.tap(loyola_campus_buildings.at(1));
+    await tester.pump();
   });
 }
