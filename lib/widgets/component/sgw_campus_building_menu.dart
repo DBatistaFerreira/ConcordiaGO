203526e60b57917af93d5f8ebbdd10abb7fc3fca
nick <nicolas_7979@yahoo.ca>
2020-02-27 14:27:10 -0500

    Fix formatting

--- lib/widgets/component/sgw_campus_building_menu.dart
+++ lib/widgets/component/sgw_campus_building_menu.dart
@@ -27,74 +27,77 @@ class SGWCampusBuildingListState extends State<SGWCampusBuildingList> {
         building['campus'] == 'SGW Campus' ? buildings.add(Building(code, building['name'])) : true);
 
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(screenHeight / 12),
-            child: AppBar(
-              title: Image.asset('assets/logo.png', height: screenHeight / 12),
-              backgroundColor: application_constants.concordiaRed,
-            )),
-        body: Column(
-          children: <Widget>[
-            Container(
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "SGW Campus Buildings",
-                      style: TextStyle(
-                        fontFamily: 'Raleway',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: application_constants.listElementTextSize,
-                      ),
-                    )),
-                color: application_constants.concordiaRed,
-                height: screenHeight / 15),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: buildings.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Container(
-                      height: application_constants.initialedIconSize,
-                      width: application_constants.initialedIconSize,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Ink(
-                          decoration: const ShapeDecoration(
-                            color: application_constants.concordiaRed,
-                            shape: CircleBorder(),
-                          ),
-                          child: Center(
-                            child: Text(
-                              buildings[index].buildingCode.length == 2
-                                  ? buildings[index].buildingCode
-                                  : buildings[index].buildingCode + ' ',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: application_constants.listElementTextSize,
-                              ),
-                              textAlign: TextAlign.center,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(screenHeight / 12),
+        child: AppBar(
+          title: Image.asset('assets/logo.png', height: screenHeight / 12),
+          backgroundColor: application_constants.concordiaRed,
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "SGW Campus Buildings",
+                style: TextStyle(
+                  fontFamily: 'Raleway',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: application_constants.listElementTextSize,
+                ),
+              ),
+            ),
+            color: application_constants.concordiaRed,
+            height: screenHeight / 15,
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: buildings.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Container(
+                    height: application_constants.initialedIconSize,
+                    width: application_constants.initialedIconSize,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Ink(
+                        decoration: const ShapeDecoration(
+                          color: application_constants.concordiaRed,
+                          shape: CircleBorder(),
+                        ),
+                        child: Center(
+                          child: Text(
+                            buildings[index].buildingCode.length == 2
+                                ? buildings[index].buildingCode
+                                : buildings[index].buildingCode + ' ',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: application_constants.listElementTextSize,
                             ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
                       ),
                     ),
-                    title: Text(
-                      buildings[index].name,
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      BlocProvider.of<MapBloc>(context)
-                          .add(CameraMoveConcordia(buildings[index].buildingCode, context));
-                    },
-                    trailing: Icon(Icons.keyboard_arrow_right),
-                  );
-                },
-              ),
+                  ),
+                  title: Text(
+                    buildings[index].name,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    BlocProvider.of<MapBloc>(context).add(CameraMoveConcordia(buildings[index].buildingCode, context));
+                  },
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                );
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
