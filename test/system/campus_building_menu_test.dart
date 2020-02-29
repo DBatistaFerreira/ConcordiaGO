8761c224d8c9a87dac4e36bdc5e9ed7b495188ff
Jake <bedardjake@gmail.com>
2020-02-29 17:56:56 -0500

    Added skeleton

--- test/system/campus_building_menu_test.dart
+++ test/system/campus_building_menu_test.dart
@@ -1,18 +1,25 @@
+import 'package:concordia_go/widgets/screens/campus_building_list_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class LoadSGWCampusBuildingList extends StatelessWidget {
+class LoadCampusBuildingListMenu extends StatelessWidget {
+  String campus;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SGWCampusBuildingList(),
+      home: CampusBuildingListMenu('SGW Campus'),
     );
+  }
+
+  LoadCampusBuildingListMenu(String campus) {
+    this.campus = campus;
   }
 }
 
 void main() {
   testWidgets('SGW Campus Building Menu', (WidgetTester tester) async {
-    await tester.pumpWidget(LoadSGWCampusBuildingList());
+    await tester.pumpWidget(LoadCampusBuildingListMenu("SGW Campus"));
 
 //    final favoritePlaces = find.byIcon(Icons.favorite);
 //    final campuses = find.byIcon(Icons.school);
