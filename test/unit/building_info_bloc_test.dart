eeccece71ddabf1e556fbc9d127b06eb10cb1333
nick <nicolas_7979@yahoo.ca>
2020-02-29 20:51:43 -0500

    Remove unnecessary test files

--- test/unit/building_info_bloc_test.dart
+++ test/unit/building_info_bloc_test.dart
@@ -1,12 +1,9 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:concordia_go/blocs/building_info_bloc/building_info_bloc.dart';
 import 'package:concordia_go/blocs/building_info_bloc/building_info_event.dart';
 import 'package:concordia_go/blocs/building_info_bloc/building_info_state.dart';
 import 'package:concordia_go/utilities/concordia_constants.dart';
-
-class MockBuildingInfoBloc extends Mock implements BuildingInfoBloc {}
 
 void main() {
   final event = ConcordiaBuildingInfo(buildings['H'].code);
