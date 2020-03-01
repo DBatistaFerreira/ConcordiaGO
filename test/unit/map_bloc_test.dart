e600aad02096a145ced187fa0486e168dfbd1c95
Michael Teolis <michaelteolis96@gmail.com>
2020-02-29 21:34:19 -0500

    Add test for map event

--- test/unit/map_bloc_test.dart
+++ test/unit/map_bloc_test.dart
@@ -1,5 +1,17 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:concordia_go/blocs/bloc.dart';
+import 'package:concordia_go/utilities/concordia_constants.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Test 1', () {});
+  final event = CameraMove(buildings['H'].coordinates, 1.0);
+
+  blocTest(
+    'Get Map Camera Movement',
+    build: () async => MapBloc(),
+    act: (bloc) {
+      return bloc.add(event);
+    },
+    expect: [isA<MapNoMarker>()],
+  );
 }
