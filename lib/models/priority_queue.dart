101d45adcb02882da8a0924013fa0df583b3789f
Michael Teolis <michaelteolis96@gmail.com>
2020-03-04 16:41:07 -0500

    Add priority queue implementation using min heap

--- lib/models/priority_queue.dart
+++ lib/models/priority_queue.dart
@@ -1,1 +1,26 @@
-class PriorityQueue {}
+import 'package:concordia_go/models/min_heap.dart';
+import 'package:concordia_go/models/edge.dart';
+
+class PriorityQueue extends MinHeap {
+  MinHeap _pq;
+
+  PriorityQueue(){
+    _pq = MinHeap();
+  }
+
+  PriorityQueue.fromList(List<Edge> list){
+    _pq = MinHeap.fromList(list);
+  }
+
+  void prioritize(){
+    _pq.heapify();
+  }
+
+  void enqueue(Edge edge){
+    _pq.insert(edge);
+  }
+
+  Edge dequeue(){
+    return _pq.delete();
+  }
+}
