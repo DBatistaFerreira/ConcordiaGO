31d00a7ef53171b47cab5dc9fa0cf0da0b76f5c8
Michael Teolis <michaelteolis96@gmail.com>
2020-03-03 21:30:08 -0500

    Add Edge tests

---
+++ test/unit/edge_test.dart
@@ -0,0 +1,62 @@
+import 'package:concordia_go/models/edge.dart';
+import 'package:concordia_go/models/node.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  final from = Node(1);
+  final to = Node(2);
+  final weight = 3;
+  final edge = Edge(from, to, weight);
+
+  // region group getters
+  group('Getters', (){
+    test('getFrom should return from node object', (){
+      expect(edge.getFrom(), from);
+    });
+
+    test('getTo should return to node object', (){
+      expect(edge.getTo(), to);
+    });
+
+    test('getWeight should return weight int', (){
+      expect(edge.getWeight(), 3);
+    });
+  });
+  // endregion group getters
+
+  // region group setters
+  group('Setters', (){
+    test('setFrom should set the from variable to the node object passed', (){
+      final setFrom = Node(4);
+
+      edge.setFrom(setFrom);
+
+      expect(edge.getFrom(), setFrom);
+    });
+
+    test('setTo should set the to variable to the node object passed', (){
+      final setTo = Node(4);
+
+      edge.setTo(setTo);
+
+      expect(edge.getTo(), setTo);
+    });
+
+    test('setWeight should set the weight variable to the int passed', (){
+      edge.setWeight(7);
+
+      expect(edge.getWeight(), 7);
+    });
+  });
+  // endregion group setters
+
+  group('Functions', (){
+    test('isEqual should return true if both edges are the same', (){
+      final notEqual = Edge(to, from, weight);
+      expect(edge.isEqual(notEqual), false);
+      expect(edge.isEqual(edge), true);
+    });
+
+    // TODO: implement rest of tests
+  });
+}
