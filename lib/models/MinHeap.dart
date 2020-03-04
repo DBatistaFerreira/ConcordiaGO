c379fe46716c205b229434bfda7516cf8d7791ed
Michael Teolis <michaelteolis96@gmail.com>
2020-03-03 20:49:25 -0500

    Refactor to min heap of Edge objects

--- lib/models/MinHeap.dart
+++ lib/models/MinHeap.dart
@@ -1,8 +1,10 @@
+import 'Edge.dart';
+
 class MinHeap {
-  List<int> _heap;
+  List<Edge> _heap;
 
   MinHeap() {
-    _heap = List<int>();
+    _heap = <Edge>[];
   }
 
   int _parent(int index) {
@@ -22,15 +24,15 @@ class MinHeap {
   }
 
   void _swap(int index1, int index2) {
-    int tmp = _heap[index1];
+    var tmp = _heap[index1];
     _heap[index1] = _heap[index2];
     _heap[index2] = tmp;
   }
 
   void _sink(int index) {
     if (!_isLeaf(index)) {
-      if (_heap[index] > _heap[_leftChild(index)] || _heap[index] > _heap[_rightChild(index)]) {
-        if (_heap[_leftChild(index)] < _heap[_rightChild(index)]) {
+      if (_heap[index].getWeight() > _heap[_leftChild(index)].getWeight() || _heap[index].getWeight() > _heap[_rightChild(index)].getWeight()) {
+        if (_heap[_leftChild(index)].getWeight() < _heap[_rightChild(index)].getWeight()) {
           _swap(index, _leftChild(index));
           _sink(_leftChild(index));
         } else {
@@ -42,27 +44,31 @@ class MinHeap {
   }
 
   void _swim(int index) {
-    while (_heap[index] > _heap[_parent(index)]) {
+    while (_heap[index].getWeight() > _heap[_parent(index)].getWeight()) {
       _swap(index, _parent(index));
       index = _parent(index);
     }
   }
 
-  void insert(int element) {
-    _heap.add(element);
-    int current = _heap.length - 1;
+  void insert(Edge edge) {
+    _heap.add(edge);
+    var current = _heap.length - 1;
     _swim(current);
   }
 
   void minHeap() {
-    for (int i = (_heap.length ~/ 2); i >= 0; i--) {
+    for (var i = (_heap.length ~/ 2); i >= 0; i--) {
       _sink(i);
     }
   }
 
-  int remove() {
-    int removed = _heap[0];
-    _heap.removeAt(0);
-    return removed;
+  Edge delete() {
+//    var deleted = _heap.removeAt(0);
+//    _sink(0);
+    return _heap.removeAt(0);
+  }
+
+  Edge peek(){
+    return _heap.first;
   }
 }
