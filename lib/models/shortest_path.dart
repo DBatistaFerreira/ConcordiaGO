110f1cc2d16dc06bd71792291a51b51e02f4a01d
Michael Teolis <michaelteolis96@gmail.com>
2020-03-22 16:27:14 -0400

    Add greedy dijkstra shortest path algorithm implementation
    and unit tests for shortest path

--- lib/models/shortest_path.dart
+++ lib/models/shortest_path.dart
@@ -8,7 +8,8 @@ class DShortestPath {
   int _distance;
   Map<String, Node> _visited;
   Map<String, Node> _unvisited;
-  // List<Node> shortest_path
+  List<Node> _shortest_path;
+  List _table;
 
   // region constructors
   DShortestPath(Graph graph, Node source, Node destination) {
@@ -17,7 +18,20 @@ class DShortestPath {
     _destination = destination;
     _distance = 0;
     _visited = <String, Node>{};
-    _unvisited = graph.getNodes();
+    _unvisited = Map.from(graph.getNodes());
+    _shortest_path = <Node>[];
+    _table = [];
+  }
+
+  DShortestPath.withNothing(){
+    _graph = null;
+    _source = null;
+    _destination = null;
+    _distance = 0;
+    _visited = <String, Node>{};
+    _unvisited = <String, Node>{};
+    _shortest_path = <Node>[];
+    _table = [];
   }
   // endregion constructors
 
@@ -38,6 +52,15 @@ class DShortestPath {
   int getDistance() {
     return _distance;
   }
+
+  List<Node> getShortestPath(){
+    return _shortest_path;
+  }
+
+  List getTable(){
+    return _table;
+  }
+
   // endregion getters
 
   // region setters
@@ -57,12 +80,118 @@ class DShortestPath {
   void setDistance(int distance) {
     _distance = distance;
   }
+
+  void setShortestPath(List<Node> shortest_path){
+    _shortest_path = shortest_path;
+  }
+
+  void setTable(List table){
+    _table = table;
+  }
   // endregion setters
+
+  // region private functions
+  void _fillTable(){
+    // set all values in the table to [node, -1, null]
+    for(var node in _graph.getNodes().values){
+      _table.add([node, -1, null]);
+    }
+    // set the distance and via values of the source node to 0 and 'source' respectively.
+    _table[_graph.getEdgeIndices().indexOf(_source.getId())][1] = 0;
+    _table[_graph.getEdgeIndices().indexOf(_source.getId())][2] = _source;
+  }
+
+  Node _getTableNode(Node node){
+    return _table[_graph.getIndex(node)][0];
+  }
+
+  int _getTableDistance(Node node){
+    return _table[_graph.getIndex(node)][1];
+  }
+
+  Node _getTableVia(Node node){
+    return _table[_graph.getIndex(node)][2];
+  }
+
+  void _updateTableDistance(Node node, int distance){
+    _table[_graph.getIndex(node)][1] = distance;
+  }
+
+  void _updateTableVia(Node node, Node via){
+    _table[_graph.getIndex(node)][2] = via;
+  }
+
+  void _updateVisited(Node node){
+    _visited[node.getId()] = _unvisited.remove(node.getId());
+  }
+
+  Node _getNextCurrentNode(){
+    var candidates = <Node>[];
+    for(var row in _table){
+      if(row[1] != -1 && !_visited.containsKey(row[0].getId())){
+        candidates.add(row[0]);
+      }
+    }
+    if(candidates.isEmpty){
+      return null;
+    }
+    var next = candidates.first;
+    for(var node in candidates){
+      if(_getTableDistance(node) < _getTableDistance(next)){
+        next = node;
+      }
+    }
+
+    return next;
+  }
+  // endregion private functions
 
   // region functions
   /*
   TODO: implement calcShortestPath() algorithm
   returns the shortest path as a list of nodes
    */
+  List<Node> calcShortestPath(){
+    _fillTable();
+
+    _greedyDijkstra();
+
+    _generateNodePathList();
+
+    return _shortest_path;
+  }
+
+  void _generateNodePathList() {
+    _distance = _getTableDistance(_destination);
+
+    var node = _destination;
+    while(!_getTableNode(node).isEqualTo(_source)){
+      _shortest_path.insert(0, node);
+      node = _getTableVia(node);
+    }
+
+    _shortest_path.insert(0, _source);
+  }
+
+  void _greedyDijkstra() {
+    var current = _source;
+    while(_unvisited.isNotEmpty){
+      if(current == null){
+        break;
+      }
+      var nodesConnectedToCurrent = _graph.getConnectedNodes(current);
+      for(var connected in nodesConnectedToCurrent){
+        var distanceFromSource = _getTableDistance(current)
+            + _graph.getWeight(current, connected);
+        if(_getTableDistance(connected) == -1 || distanceFromSource < _getTableDistance(connected)){
+          _updateTableDistance(connected, distanceFromSource);
+          _updateTableVia(connected, current);
+        }
+      }
+      _updateVisited(current);
+      current = _getNextCurrentNode();
+    }
+  }
+
   // endregion functions
 }
--- test/unit/shortest_path_test.dart
+++ test/unit/shortest_path_test.dart
@@ -5,12 +5,114 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:concordia_go/utilities/concordia_constants.dart' as cc;
 
 void main() {
-  // region group
-  group('description', () {
-    // region group general fixtures
+  // region constructor tests
+  group('constructors', () {
+    // region constructor tests general fixtures
     final g = Graph('H8', cc.edges['H8'], cc.edge_indices['H8']);
-    // endregion group general fixtures
-    test('description', () {});
+    final src = Node(cc.edge_indices['H8'][48]); // class 805
+    final dst = Node(cc.edge_indices['H8'][82]); // class 862
+    // endregion constructor tests general fixtures
+
+    test('constructor should initialize the variables with the values passed', () {
+      final sp = DShortestPath(g, src, dst);
+
+      expect(sp.getGraph(), g);
+      expect(sp.getSource(), src);
+      expect(sp.getDestination(), dst);
+      expect(sp.getDistance(), 0);
+    });
   });
-  // endregion group
+  // endregion constructor tests
+  // region getter tests
+  group('getters', () {
+    // region getter tests general fixtures
+    final g = Graph('H8', cc.edges['H8'], cc.edge_indices['H8']);
+    final src = Node(cc.edge_indices['H8'][48]); // class 805
+    final dst = Node(cc.edge_indices['H8'][82]); // class 862
+    final sp = DShortestPath(g, src, dst);
+    // endregion getter tests general fixtures
+
+    test('getGraph should return the graph of this shortest path', () {
+      expect(sp.getGraph(), g);
+    });
+
+    test('getSource should return the source node of this shortest path', () {
+      expect(sp.getSource(), src);
+    });
+
+    test('getDestination should return the destination node of this shortest path', () {
+      expect(sp.getDestination(), dst);
+    });
+
+//    test('getDistance should return the distance of this shortest path', () {
+//      expect(sp.getDistance(), 0);
+//      sp.setDistance(23);
+//      expect(sp.getDistance(), 23);
+//    });
+  });
+  // endregion getter tests
+  // region setter tests
+  group('setters', (){
+    // region setter tests general fixtures
+      final sp = DShortestPath.withNothing();
+    // endregion setter tests general fixtures
+    test('setGraph should set the graph to the graph passed', (){
+      final g = Graph.withNothing();
+      sp.setGraph(g);
+      expect(g.getName(), '');
+      expect(g.getNodes().isEmpty, true);
+      expect(g.getEdges().isEmpty, true);
+      expect(g.getEdgeIndices().isEmpty, true);
+    });
+
+    test('setSource should set the source node to the node passed', (){
+      final source = Node('123');
+      sp.setSource(source);
+      expect(sp.getSource(), source);
+      expect(sp.getSource().getId(), '123');
+    });
+
+    test('setDestination should set the destination node to the node passed', (){
+      final destination = Node('321');
+      sp.setDestination(destination);
+      expect(sp.getDestination(), destination);
+      expect(sp.getDestination().getId(), '321');
+    });
+
+    test('setDistance should set the distance to the value passed', (){
+      final distance = 34;
+      sp.setDistance(distance);
+      expect(sp.getDistance(), distance);
+    });
+  });
+  // endregion setter tests
+
+  group('calcShortestPath', (){
+    // region calcShortestPath test general fixtures
+
+    // endregion calcShortestPath test general fixtures
+    test('calcShortestPath should return a list of the shortest path nodes', (){
+      final g = Graph('H8', cc.edges['H8'], cc.edge_indices['H8']);
+      g.setNodesFromEdgeIndices(cc.edge_indices['H8']);
+      final sp = DShortestPath(g, g.getNodes()['000003'], g.getNodes()['000000']);
+      expect(sp.getShortestPath().isEmpty, true);
+      sp.calcShortestPath();
+      expect(sp.getShortestPath().first.isEqualTo(Node('000003')), true);
+      expect(sp.getShortestPath().last.isEqualTo(Node('000000')), true);
+      expect(sp.getDistance(), 11);
+      final sp2 = DShortestPath(g, g.getNodes()['100805'], g.getNodes()['100862']);
+      expect(sp2.getShortestPath().isEmpty, true);
+      sp2.calcShortestPath();
+      expect(sp2.getShortestPath().first.isEqualTo(Node('100805')), true);
+      expect(sp2.getShortestPath().last.isEqualTo(Node('100862')), true);
+      expect(sp2.getDistance(), 28);
+      final sp3 = DShortestPath(g, g.getNodes()['100805'], g.getNodes()['100865']);
+      expect(sp3.getShortestPath().isEmpty, true);
+      sp3.calcShortestPath();
+      expect(sp3.getShortestPath().first.isEqualTo(Node('100805')), true);
+      expect(sp3.getShortestPath().last.isEqualTo(Node('100865')), true);
+      expect(sp3.getDistance(), 16);
+      expect(sp.getDistance(), 11);
+    });
+  });
 }
