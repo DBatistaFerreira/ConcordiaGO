1eea1c5a6ff3cf94a236e46fd473835e8bf515e1
Michael Teolis <michaelteolis96@gmail.com>
2020-03-26 13:45:10 -0400

    Create direction request class

---
+++ lib/models/direction_request.dart
@@ -0,0 +1,55 @@
+
+class DirectionRequest{
+  List<String> _buildings;
+  List<String> _rooms;
+
+  DirectionRequest(List<String> buildings, List<String> rooms){
+    _buildings = buildings;
+    _rooms = rooms;
+  }
+
+  DirectionRequest.withNothing(){
+    _buildings = <String>[];
+    _rooms = <String>[];
+  }
+
+  void setBuildings(List<String> buildings){
+    _buildings = buildings;
+  }
+
+  void setRooms(List<String> rooms){
+    _rooms = rooms;
+  }
+
+  void setRoomSource(String room){
+    _rooms[0] = room;
+  }
+
+  void setRoomDestination(String room){
+    _rooms[1] = room;
+  }
+
+  List<String> getBuildings(){
+    return _buildings;
+  }
+
+  List<String> getRooms(){
+    return _rooms;
+  }
+
+  String getBuildingSource(){
+    return _buildings[0];
+  }
+
+  String getBuildingDestination(){
+    return _buildings[1];
+  }
+
+  String getRoomSource(){
+    return _rooms[0];
+  }
+
+  String getRoomDestination(){
+    return _rooms[1];
+  }
+}
