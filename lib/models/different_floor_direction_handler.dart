6c4a1e9f44214ed08806e0ba9ab9ce3a30c9e2c0
Michael Teolis <michaelteolis96@gmail.com>
2020-03-27 19:07:37 -0400

    Add implementation for different floor handler shortest path calculation

--- lib/models/different_floor_direction_handler.dart
+++ lib/models/different_floor_direction_handler.dart
@@ -1,5 +1,9 @@
 import 'package:concordia_go/models/direction_handler.dart';
 import 'package:concordia_go/models/direction_request.dart';
+import 'package:concordia_go/models/shortest_path.dart';
+import 'package:concordia_go/models/graph.dart';
+import 'package:concordia_go/models/node.dart';
+import 'package:concordia_go/utilities/concordia_constants.dart' as cc;
 
 class DifferentFloorDirectionHandler implements DirectionHandler {
 
@@ -13,7 +17,30 @@ class DifferentFloorDirectionHandler implements DirectionHandler {
   @override
   void handle(DirectionRequest request) {
     if(canHandle(request)){
-      // TODO: implement the process of different floor direction handling
+      var source_building_code = request.source.building.code + request.source.floor;
+      var destination_building_code = request.destination.building.code + request.destination.floor;
+
+      var source_graph = Graph(source_building_code, cc.edges[source_building_code], cc.edge_indices[source_building_code]);
+      source_graph.setNodesFromEdgeIndices(cc.edge_indices[source_building_code]);
+
+      var destination_graph = Graph(destination_building_code, cc.edges[destination_building_code], cc.edge_indices[destination_building_code]);
+      destination_graph.setNodesFromEdgeIndices(cc.edge_indices[destination_building_code]);
+
+      var escalator_node;
+      if(int.parse(request.source.floor) > int.parse(request.destination.floor)){
+        escalator_node = Node('130000');
+      }
+      else {
+        escalator_node = Node('130001');
+      }
+
+      var source_algorithm = DShortestPath(source_graph, request.source.node, escalator_node);
+      var destination_algorithm = DShortestPath(destination_graph, escalator_node, request.destination.node);
+
+      var source_shortest_path = source_algorithm.calcShortestPath();
+      var destination_shortest_path = destination_algorithm.calcShortestPath();
+
+      // TODO: implement passing of shortest path to indoor directions view and draw path on SVG
     }
     else{
       _next_handler.handle(request);
@@ -24,7 +51,7 @@ class DifferentFloorDirectionHandler implements DirectionHandler {
   @override
   bool canHandle(DirectionRequest request) {
     return (request.source.isIndoor() && request.destination.isIndoor())
-        && (request.source.building == request.destination.building)
+        && (request.source.building.code == request.destination.building.code)
         && (request.source.floor != request.destination.floor);
   }
 }
