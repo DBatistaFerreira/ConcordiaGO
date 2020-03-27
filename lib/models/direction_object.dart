9757c884cfc1a9676efe4a3db81046ae7b6a01ee
Michael Teolis <michaelteolis96@gmail.com>
2020-03-27 17:40:06 -0400

    Add mode of transport variable and refactor node import

--- lib/models/direction_object.dart
+++ lib/models/direction_object.dart
@@ -1,7 +1,6 @@
-
-import 'dart:html';
-
+import 'package:concordia_go/models/node.dart';
 import 'package:concordia_go/models/concordia_building.dart';
+import 'package:concordia_go/utilities/direction.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Dobject {
@@ -10,6 +9,7 @@ class Dobject {
   LatLng _coordinates;
   String _floor;
   String _name;
+  ModeOfTransport _transport_mode;
 
   Dobject(){
     _node = null;
@@ -17,6 +17,7 @@ class Dobject {
     _coordinates = null;
     _floor = null;
     _name = null;
+    _transport_mode = null;
   }
 
   Dobject.indoor(Node node, ConcordiaBuilding building, String floor){
@@ -25,12 +26,13 @@ class Dobject {
     _floor = floor;
   }
 
-  Dobject.outdoor({Node node, ConcordiaBuilding building, LatLng coordinates, String floor, String name}){
+  Dobject.outdoor({Node node, ConcordiaBuilding building, LatLng coordinates, String floor, String name, ModeOfTransport transport_mode}){
     _node = node;
     _building = building;
     _coordinates = coordinates;
     _floor = floor;
     _name = name;
+    _transport_mode = transport_mode;
   }
 
   Node get node => _node;
@@ -38,45 +40,54 @@ class Dobject {
   LatLng get coordinates => _coordinates;
   String get floor => _floor;
   String get name => _name;
+  ModeOfTransport get transport_mode => _transport_mode;
 
-  void setNode(Node node){
+  set node(Node node){
     _node = node;
   }
 
-  void setBuilding(ConcordiaBuilding building){
+  set building(ConcordiaBuilding building){
     _building = building;
   }
 
-  void setCoordinates(LatLng coordinates){
+  set coordinates(LatLng coordinates){
     _coordinates = coordinates;
   }
 
-  void setFloor(String floor){
+  set floor(String floor){
     _floor = floor;
   }
 
-  void setName(String name){
+  set name(String name){
     _name = name;
   }
 
+  set transport_mode(ModeOfTransport transport_mode){
+    _transport_mode = transport_mode;
+  }
+
   bool hasNode(){
-    return (_node == null);
+    return (_node != null);
   }
 
   bool hasBuilding(){
-    return (_building == null);
+    return (_building != null);
   }
 
   bool hasCoordinates(){
-    return (_coordinates == null);
+    return (_coordinates != null);
   }
 
   bool hasFloor(){
-    return (_floor == null);
+    return (_floor != null);
   }
 
   bool hasName(){
-    return (_name == null);
+    return (_name != null);
+  }
+
+  bool hasTransportMode(){
+    return (_transport_mode != null);
   }
 
 }
