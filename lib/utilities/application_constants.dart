bbe101fdbb00739c79909f1cc829017665fc5d7c
Daniel <danielwiktorczyk@gmail.com>
2020-04-08 18:53:18 -0400

    Update Application Constants and pubspec with Assets

--- lib/utilities/application_constants.dart
+++ lib/utilities/application_constants.dart
@@ -6,6 +6,15 @@ const String applicationName = 'ConcordiaGO';
 const Color concordiaRed = Color(0xff800206);
 
 const String concordiaGOHeader = 'assets/headers/ConcordiaGOHeader.png';
+const String concordiaGOHeader_Red = concordiaGOHeader;
+const String concordiaGOHeader_Blue = 'assets/headers/ConcordiaGOHeader_Blue.png';
+const String concordiaGOHeader_DarkGreen = 'assets/headers/ConcordiaGOHeader_DarkGreen.png';
+const String concordiaGOHeader_Magenta = 'assets/headers/ConcordiaGOHeader_Magenta.png';
+const String concordiaGOHeader_NeonGreen = 'assets/headers/ConcordiaGOHeader_NeonGreen.png';
+const String concordiaGOHeader_Orange = 'assets/headers/ConcordiaGOHeader_Orange.png';
+const String concordiaGOHeader_Purple = 'assets/headers/ConcordiaGOHeader_Purple.png';
+const String concordiaGOHeader_Yellow = 'assets/headers/ConcordiaGOHeader_Yellow.png';
+
 const String concordiaGOLogo = 'assets/logos/ConcordiaGOLogo.png';
 
 const double listElementTextSize = 24.0;
