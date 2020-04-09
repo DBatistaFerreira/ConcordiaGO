77aed5c00a35f3a3c49c08a793f5f28ad8289afc
Dariush Noorzada <dariush.noorzada@mail.concordia.ca>
2020-04-09 17:39:25 -0400

    Add preferred washroom shared preferrence storing

--- lib/services/shared_preferences_service.dart
+++ lib/services/shared_preferences_service.dart
@@ -5,6 +5,7 @@ class SharedPreferencesService {
 
   /// Instantiation of the SharedPreferences library
   static const String _kPrioritizeElevators = 'prioritizeElevators';
+  static const String _kPreferredWashroom = 'prioritizeWashroom';
 
   // Getter for the prioritize elevator boolean
   static Future<bool> getPrioritizeElevatorBool() async {
@@ -13,10 +14,24 @@ class SharedPreferencesService {
     return prefs.getBool(_kPrioritizeElevators) ?? false;
   }
 
+  // Getter for the preferred bathroom
+  static Future<String> getPreferredWashroom() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(_kPreferredWashroom) ?? 'Male';
+  }
+
   // Setter for the prioritize elevator boolean
   static Future<bool> setPrioritizeElevatorBool(bool value) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     return prefs.setBool(_kPrioritizeElevators, value);
+  }
+
+  // Setter for the preferred bathroom
+  static Future<bool> setPreferredWashroom(String value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setString(_kPreferredWashroom, value);
   }
 }
